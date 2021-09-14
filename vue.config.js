2d756988e5985e3a8ee9a587016ff794a6867309
SparrowMike <michal.m_wroblewski@hotmail.com>
2021-09-14 23:54:02 +0800

    feat: 🎸 added proxy

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "https://localhost:5000",
+        changeOrigin: true,
+        logLevel: "debug",
+        pathRewrite: { "^/api": "/" },
+      },
+    },
+  },
+};
