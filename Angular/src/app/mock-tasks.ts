f4129ea025890a403a0aee3d3245b89bf8d8eed7
SparrowMike <michal.m_wroblewski@hotmail.com>
2021-11-29 13:17:51 +0800

    feat: 🎸 added mock data and interface

---
+++ Angular/src/app/mock-tasks.ts
@@ -0,0 +1,16 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'may 5th at 2:30',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'may 8th at 4:30',
+    reminder: false,
+  },
+];
